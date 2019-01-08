9e07183007ed68f9eacb94762b87b59bc392d4a2
AlejandroFabianCampos <alejandrofabiancampos2000@gmail.com>
2019-01-08 14:59:55 -0300

    Updates

---
+++ precache-manifest.4206f18eb6168f6d8e42af9be94a0ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "480a4d2c579112adcaa1",
+    "url": "/la-cosa/static/js/runtime~main.480a4d2c.js"
+  },
+  {
+    "revision": "46a42e0b17ad1614fc30",
+    "url": "/la-cosa/static/js/main.46a42e0b.chunk.js"
+  },
+  {
+    "revision": "5a5fdfae8529277ebc3e",
+    "url": "/la-cosa/static/js/1.5a5fdfae.chunk.js"
+  },
+  {
+    "revision": "46a42e0b17ad1614fc30",
+    "url": "/la-cosa/static/css/main.5faf52b3.chunk.css"
+  },
+  {
+    "revision": "5a5fdfae8529277ebc3e",
+    "url": "/la-cosa/static/css/1.dc5f629d.chunk.css"
+  },
+  {
+    "revision": "faf390430701f011daaa5f6d21c558e5",
+    "url": "/la-cosa/index.html"
+  }
+];
